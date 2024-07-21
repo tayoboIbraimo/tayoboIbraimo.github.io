d8ba586b56e9cdd2d717a2bf6dc2effc2e420305
Nelson Alexandre Mutane <nelson.mutane@uzambeze.ac.mz>
2024-07-21 16:45:35 +0200

    vue ssg

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import { ViteSSG } from 'vite-ssg/single-page'
 import './app.css'
 import App from './App.vue'
-import 'flowbite'
-
 // `export const createApp` is required instead of the original `createApp(App).mount('#app')`
 export const createApp = ViteSSG(App)
